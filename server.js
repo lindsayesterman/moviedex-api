ac525fb6e532dffae59ae7767879b2f4857cd5dc
Lindsay Esterman <lindsayesterman@gmail.com>
2020-08-24 14:29:45 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
@@ -47,7 +58,7 @@ function handleGetMovies(req, res) {
             
 app.get('/movie', handleGetMovies)
             
-const PORT = 8000
+const PORT = process.env.PORT || 8000
             
 
 app.listen(PORT, () => {
